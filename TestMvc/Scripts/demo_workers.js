00c6d8b07476128ffe73ea8f6ef27746481d6643
daniuchens <daniuchens@gmail.com>
2015-12-08 15:58:58 +0800

    web worker 測試

---
+++ TestMvc/Scripts/demo_workers.js
@@ -0,0 +1,18 @@
+﻿var i;
+
+function timedCount() {
+    if (typeof (i) == "undefined")
+    {
+        i = 0;
+    } else {
+        i = i + 1;
+    }
+    postMessage(i);
+    setTimeout("timedCount()", 500);
+}
+
+onmessage = function (event) {
+    i = event.data;
+}
+
+timedCount();
